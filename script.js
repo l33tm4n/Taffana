5c678511fd7eef828af496656d8c43a7e1e52b25
mahesa <157023462+l33tm4n@users.noreply.github.com>
2025-04-28 13:15:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no yahaha-btn");
+const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Aaaaa, I like you too princess";
@@ -12,12 +12,12 @@ yesBtn.addEventListener("click", () => {
 
 noBtn.addEventListener("mouseover", () => {
   const noBtnRect = noBtn.getBoundingClientRect();
-  const maxX = window.innerWidth - no yahahaBtnRect.width;
-  const maxY = window.innerHeight - no yahahaBtnRect.height;
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
 
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
 
-  no yahahaBtn.style.left = randomX + "px";
-  no yahahaBtn.style.top = randomY + "px";
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
 });
